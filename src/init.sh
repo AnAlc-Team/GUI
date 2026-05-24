2b3704b005c0986c29222cb9a206b766046693fc
Spiros Papadopoulos <140538195+Papadopspiros@users.noreply.github.com>
2026-05-24 20:40:17 +0300

    Set up virtual environment and install libraries

--- src/init.sh
+++ src/init.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+#Create a virtual environment
+python3 -m venv .venv
+
+#Activate the virtual environment
 source .venv/bin/activate
 
+#Install libraries
+pip install nicegui requests opencv-python
+
+#Run the script
 python3 gui.py
